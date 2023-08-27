62fdd150e82774df7f0b50308bf9864037eb5dbe
Rekai Musuka <rekai@musuka.dev>
2023-08-26 22:06:40 -0500

    feat(avr): implement rcall

--- src/avr/rjmp.zig
+++ src/avr/rjmp.zig
@@ -10,10 +10,16 @@ pub fn handler(comptime c: bool) InstrFn {
     return struct {
         fn inner(cpu: *Cpu, opcode: u16) void {
             const PcT = @TypeOf(cpu.pc);
-            if (c) cpu.panic("TODO: implement RCALL", .{});
 
             const k: u12 = @intCast(opcode & 0x0FFF);
-            log.debug("rjmp {}", .{@sizeOf(u16) * @as(i12, @bitCast(k))});
+            log.debug("r{s} {}", .{ if (c) "call" else "jmp", @sizeOf(u16) * @as(i12, @bitCast(k)) });
+
+            if (c) {
+                const ret_addr = cpu.pc + 1;
+
+                cpu.bus.write(PcT, .data, cpu.sp, ret_addr);
+                cpu.sp -= @sizeOf(PcT);
+            }
 
             cpu.pc +%= sext(PcT, u12, k) +% 1;
 
