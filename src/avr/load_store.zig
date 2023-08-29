dbadd1b26c8275bfb1dcc855111173f6cd76c843
Rekai Musuka <rekai@musuka.dev>
2023-08-28 21:25:36 -0500

    feat(avr): implement LDS/STS

--- src/avr/load_store.zig
+++ src/avr/load_store.zig
@@ -10,8 +10,29 @@ pub fn handler(comptime s: bool, comptime op: u4) InstrFn {
     return struct {
         fn inner(cpu: *Cpu, opcode: u16) void {
             const ret = bstr.extract("100100-ddddd----", opcode);
+            // TODO: care about RAMPD
 
             switch (op) {
+                0b0000 => {
+                    // FIXME: determine the exact behaviour here
+                    const k = cpu.pipeline.stage orelse cpu.panic("prefetcher stopped working...", .{});
+                    cpu.pc += 2;
+                    cpu.pipeline.flushed = true;
+
+                    if (s) {
+                        // STS
+                        const rr = ret.d;
+                        log.debug("sts 0x{X:0>4}, r{}", .{ k, rr });
+
+                        cpu.bus.write(u8, .data, k, cpu.r[rr]);
+                    } else {
+                        // LDS
+                        const rd = ret.d;
+                        log.debug("lds r{}, 0x{X:0>4}", .{ rd, k });
+
+                        cpu.r[rd] = cpu.bus.read(u8, .data, k);
+                    }
+                },
                 0b1111 => {
                     log.debug("{s} r{}", .{ if (s) "push" else "pop", ret.d });
 
